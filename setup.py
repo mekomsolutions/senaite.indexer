7cfb8769cbaf483202408bd28e5578b11700c5c1
ruhanga <ruhanganathan@gmail.com>
2022-04-05 18:21:11 +0300

    OZ-50: Updating add-on version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='senaite.indexer',
-    version='1.0a1',
+    version='1.0.0',
     description="An add-on to index senaite catalogs",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
